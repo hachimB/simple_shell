9122572a2f0633742506a8887045f4cdb6651b72
Abderrahmane369 <cszamasu@gmail.com>
2023-08-12 14:53:04 -0700

    STRFUNCTION

---
+++ strfunc.h
@@ -0,0 +1,32 @@
+#ifndef STRFUNCS
+#define STRFUNCS
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j;
+	
+	while (s1[i] != '\0')
+		i++;
+
+	while (s2[i] != '\0')
+		i++;
+	
+	for (j = 0; j <= i; j++)
+	{
+		if (s1[j] != s2[j])
+		return (s1[j] - s2[j]);
+	}
+
+	return (0);
+}
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+#endif
