02716c33a12fd6e752d2fd4b21561b502af7788a
hachimB <boubacarhachim@gmail.com>
2023-08-16 16:09:06 +0100

    getenv

--- lib_funcs.h
+++ lib_funcs.h
@@ -8,12 +8,13 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <errno.h>
-
+extern char **environ;
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 char **inpToArray(char *inp);
 void sigint_handler(int signum);
 void ststp_handler(int signum);
 char *int2str(unsigned int n);
+char *_getenv(const char *name);
 
 #endif
