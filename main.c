bd196b8d3c8936eeb3091301b0ea3facc3709149
Abderrahmane369 <133513357+Abderrahmane369@users.noreply.github.com>
2023-08-21 23:52:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,143 +8,139 @@
  * Return: 0 Success
  */
 
+void frees(char **PDIR, char **ARGS, char *path, ...)
+{
+	char *farg;
+
+	va_list vl;
+	va_start(vl, path);
+
+	superFree_(PDIR);
+	free(path);
+	superFree_(ARGS);
+
+	farg = va_arg(vl, char *);
+
+	while (farg)
+	{
+		free(farg);
+		farg = va_arg(vl, char *);
+	}
+
+	va_end(vl);
+}
+
 int main(int argc, char **argv, char **env)
 {
 	pid_t _pid;
 	size_t n = 0;
 	ssize_t l;
 	int mode = isatty(0), _status, _ch_exit_status;
-	unsigned int _cmd_count_ = 1;
-	char **args, *_input_line_ = NULL, *_nospace_, *_str_cc_;
+	unsigned int _cmd_count_ = 0, envLen = 0;
+	char **args, *_INPUT__ = NULL, *_str_cc_, *_PATH = _getenv("PATH")? _getenv("PATH") : "_",
+				 **_PATHDIR_, *path;
 
 	signal(SIGINT, sigint_handler);
 	signal(SIGTSTP, ststp_handler);
-
 	(void)argc;
-	(void)argv;
- 	while(1)
+	while (1)
 	{
-		if (mode)
+		if (mode) write(STDOUT_FILENO, "$ ", 2);
+			
+		if ((l = getline(&_INPUT__, &n, stdin)) == -1)
 		{
-			if(write(STDOUT_FILENO, "$ ", 2) < 0)
-			{
-				perror("Write");
-				continue;
-			}
-		}
-		if ((l = getline(&_input_line_, &n, stdin)) == -1)
-		{
-			if (!_input_line_)
-			{
-				perror("Allocation failed");
-				exit(EXIT_FAILURE);
-			}
-			/*if (errno != 0)
-				{
-					perror("Error");
-					free(_input_line_);
-					exit(EXIT_FAILURE);
-				}*/
-			if (mode == 1)
-			{
-				if(write(STDOUT_FILENO, "\n", 1) < 0)
-				{
-					perror("Write");
-					free(_input_line_);
-					continue;
-				}
-			}
+			if (mode) write(STDOUT_FILENO, "\n", 1);
 
-			free(_input_line_);
+			free(_INPUT__);
 			exit(errno);
 		}
 
+		while (*env) envLen++, env++;
 
-		_input_line_[l - 1] = '\0';
-		_nospace_ = _input_line_;
 
-		while (*_nospace_ == '\t' || *_nospace_ == ' ')
-			_nospace_++;
+		args = toArray(_INPUT__, " \t\n");
+		_PATHDIR_ = toArray(_PATH, ":");
 
-		args = inpToArray(_nospace_);
-
-		if (_strcmp("exit", _nospace_) == 0)
-		{
-			free(args);
-			_nospace_ = NULL;
-			free(_input_line_);
-			_input_line_ = NULL;
-
-			exit(errno);
-		}
-
-		/*if (_strcmp("env", _nospace_) == 0)
-                {
-			d_env();
-
-			}*/
-
-		_pid = fork();
-
-		if (_pid < 0)
-		{
-			perror("Fork");
-			free(args);
-			free(_input_line_);
-			_nospace_ = NULL;
-
-			exit(EXIT_FAILURE);
-		}
-
-		if (_pid == 0) {
-			_str_cc_ = int2str(_cmd_count_);
-
-			if(*_nospace_ != '\0' && execve(_nospace_, args, env) == -1)
-			{
-				if (write(STDERR_FILENO, argv[0], _strlen(argv[0])) < 0 ||
-				  write(STDERR_FILENO, ": ", 2) < 0 ||
-				  write(STDERR_FILENO, _str_cc_, _strlen(_str_cc_)) < 0 ||
-				  write(STDERR_FILENO, ": ", 2) < 0 ||
-				  write(STDERR_FILENO, _nospace_, _strlen(_nospace_)) < 0 ||
-				  write(STDERR_FILENO, ": not found\n", 12) < 0)
-				  {
-				 	perror("Write");
-				  	free(args);
-				  	free(_input_line_);
-				  	free(_str_cc_);
-				  	_nospace_ = _str_cc_ = _input_line_ = NULL;
-				  	exit(EXIT_FAILURE);
-				  }
-
-			}
-			
-			free(args);
-			free(_input_line_);
-			free(_str_cc_);
-			_nospace_ = _str_cc_ = _input_line_ = NULL;
-
-			exit(127);
-		}
-			
-		if(wait(&_status) == -1)
-		{
-			perror("Wait");
-			exit(EXIT_FAILURE);
-		}
-
-		if (WIFEXITED(_status))
-		{
-			_ch_exit_status = WEXITSTATUS(_status);
-		}
+		path = toPath(_PATHDIR_, args[0]); 
 
 		_cmd_count_++;
-		free(args);
-		free(_input_line_);
+
+		if (_strcmp("exit", args[0]) == 0)
+		{
+			if (args[1])
+			{
+				if (str2int(args[1]) >= 0)
+				{
+					errno = str2int(args[1]);
+					frees(_PATHDIR_, args, path, _INPUT__, NULL);
+					_PATHDIR_ =  args = (char **)NULL;
+					path = _INPUT__ = (char *)NULL;
+					exit (errno);
+				}
+				_str_cc_ = int2str(_cmd_count_);
+				printerr(argv[0], _str_cc_, args[0], ": Illegal number: ", args[1]);
+				frees(_PATHDIR_, args, path, _INPUT__, _str_cc_, NULL);
+				_PATHDIR_ =  args = (char **)NULL;
+				path = _INPUT__ = _str_cc_ = (char *)NULL;
+				errno = 2;
+				continue;
+			}
+
+			frees(_PATHDIR_, args, path, _INPUT__, NULL);
+			_PATHDIR_ =  args = (char **)NULL;
+			path = _INPUT__ = (char *)NULL;
+			exit(_cmd_count_ != 1 ? errno : 0);
+		}
+		if (_strcmp("env", args[0]) == 0)
+		{
+			d_env();
+			errno = 0;
+			continue;
+		}
+
+		if (access(path, F_OK) != 0 || *args[0] == '\0' )
+		{
+			errno = 0;
+
+			if (*args[0] != '\0')
+			{
+				_str_cc_ = int2str(_cmd_count_);
+				printerr(argv[0], _str_cc_, args[0], ": not found", NULL);
+				free(_str_cc_);
+				_str_cc_ = NULL;
+				errno = 127;
+			}
+			frees(_PATHDIR_, args, path, _INPUT__, NULL);
+			_PATHDIR_ = args = (char **)NULL;
+			path = _INPUT__ = (char *)NULL;
+			continue;
+		}
+		_pid = fork();
+		if (_pid < 0)
+		{
+			perror("Fork"), frees(_PATHDIR_, args, path, _INPUT__, NULL);
+			_PATHDIR_ =  args = (char **)NULL;
+			path = _INPUT__ = _str_cc_ = (char *)NULL;
+			exit(EXIT_FAILURE);
+		}
+		if (_pid == 0)
+		{
+			if (execve(path, args, env) == -1)
+			{
+
+				frees(_PATHDIR_, args, path, _INPUT__, NULL);
+				_PATHDIR_ =  args = (char **)NULL;
+				path = _INPUT__ = (char *)NULL;
+				exit(2);
+			}
+		}
+
+		wait(&_status);
+		if (WIFEXITED(_status)) _ch_exit_status = WEXITSTATUS(_status);
 		errno = _ch_exit_status;
-		
-		_input_line_ = _nospace_ = NULL;
-
+		frees(_PATHDIR_, args, path, _INPUT__, NULL);
+		_PATHDIR_ =  args = (char **)NULL;
+		path = _INPUT__ = (char *)NULL;
 	}
-
 	return (0);
 }
