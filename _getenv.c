8784ebe00c4c0c87eed78d9a55196654a56b7269
Abderrahmane369 <133513357+Abderrahmane369@users.noreply.github.com>
2023-08-21 23:55:58 +0100

    Update _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -4,22 +4,28 @@
  * @name: string.
  * Return: a string.
  */
+
 char *_getenv(const char *name)
 {
-int i = 0;
-char *c_env = environ[i];
-char *var;
-char *val;
-for (i = 0; environ[i] != NULL; i++)
-{
-c_env = environ[i];
-var = strtok(c_env, "=");
-val = strtok(NULL, "=");
-if (var != NULL && val != NULL)
-{
-if (_strcmp(var, (char *)name) == 0)
-return (val);
-}
-}
-return (NULL);
+    size_t name_length;
+    int i;
+    char *c_env;
+
+    if (name == NULL || environ == NULL)
+    {
+        return NULL;
+    }
+
+    name_length = strlen(name);
+
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        c_env = environ[i];
+        if (_strncmp(c_env, name, name_length) == 0 && c_env[name_length] == '=')
+        {
+            return c_env + name_length + 1;
+        }
+    }
+
+    return NULL;
 }
