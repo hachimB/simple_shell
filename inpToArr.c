cdae1699f3f255b9412ecaae58e66cf240317b7a
Abderrahmane369 <133513357+Abderrahmane369@users.noreply.github.com>
2023-08-21 23:57:23 +0100

    Update inpToArr.c

--- inpToArr.c
+++ inpToArr.c
@@ -6,28 +6,79 @@
  * Return: arr
  */
 
-char **inpToArray(char *inp)
+char **toArray(char *str, char *_sep_)
 {
-char *_sp = " \t", *i = inp, *in = inp, *tokn, **arr;
-int n = 1, c = 0;
-while (*i)
-{
-if ((*i == ' ' && *(i + 1) != ' ' && *(i + 1) != '\0')
-|| (*i == '\r' && *(i + 1) != ' ' && *(i + 1) != '\0')
-|| (*i == '\t' && *(i + 1) != ' ' && *(i + 1) != '\0')
-|| (*i == '\n' && *(i + 1) != ' ' && *(i + 1) != '\0'))
-n++;
-i++;
+    char *_str = strdup(str), *tokn, **arr;
+    int n = 0, c = 0;
+
+    if (!_str)
+        return (NULL);
+
+    tokn = NULL;
+
+    if (strcmp(_str, "") != 0)
+        tokn = strtok(_str, _sep_);
+   
+
+    while (tokn)
+    {
+        tokn = strtok(NULL, _sep_);
+        n++;
+    }
+
+    free(_str);
+
+    _str = strdup(str);
+    if (!_str)
+        return (NULL);
+
+    if (strcmp(_str, "") != 0)
+        tokn = strtok(_str, _sep_);
+
+    n = n ? n : 1;
+
+    arr = malloc(sizeof(char *) * (n + 1));
+    if (!arr)
+        return (NULL);
+
+    while (tokn)
+    {
+        arr[c] = strdup(tokn);
+        if (!arr[c])
+            return (NULL);
+
+        tokn = strtok(NULL, _sep_);
+        c++;
+    }
+
+    arr[0] = c ? arr[0] : strdup("");
+    arr[c ? c : 1] = (char *)NULL;
+
+    free(_str);
+    return (arr);
 }
-tokn = strtok(in, _sp);
-arr = malloc(sizeof(char *) * (n + 1));
-while (tokn)
+
+char *toPath(char **_PDIR_, char *cmd)
 {
-arr[c] = tokn;
-c++;
-tokn = strtok(NULL, _sp);
-}
-arr[0] = in;
-arr[c != 0 ? c : 1] = NULL;
-return (arr);
+    char **_PATHDIR_ = _PDIR_, *_CONC;
+
+    while (*_PATHDIR_)
+    {
+        _CONC = malloc(_strlen(*_PATHDIR_) + _strlen(cmd) + 2);
+        if (!_CONC) return (NULL);
+
+        strcpy(_CONC, *_PATHDIR_);
+        strcat(_CONC, "/");
+        strcat(_CONC, cmd);
+
+        if (access(_CONC, F_OK) == 0)
+            return (_CONC);
+
+        free(_CONC);
+        _CONC = NULL;
+        _PATHDIR_++;
+    }
+    _CONC = strdup(cmd);
+
+    return (_CONC);
 }
