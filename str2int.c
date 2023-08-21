2dee10d339489036350d16737c7687e9a4e4c803
Abderrahmane369 <133513357+Abderrahmane369@users.noreply.github.com>
2023-08-22 00:01:35 +0100

    Create str2int.c

---
+++ str2int.c
@@ -0,0 +1,33 @@
+#include "lib_funcs.h"
+
+/**
+ * str2int - str to int
+ * @str: str
+ * Return: int
+*/
+
+int str2int(char *str)
+{
+    char *s = str;
+    int t = 1, sum = 0;
+
+    while (*s)
+    {
+        s++;
+    }
+
+    s--;
+
+    while (*s)
+    {
+        if (*s < '0' || *s > '9')
+            return (-1);
+
+        sum += (*s - 48) * t;
+        t *= 10;
+
+        s--;
+    }
+
+    return (sum);
+}
