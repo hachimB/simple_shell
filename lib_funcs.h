26112d6095c61a68a72c898c8cb4b2662a3295d7
Abderrahmane369 <133513357+Abderrahmane369@users.noreply.github.com>
2023-08-22 05:26:49 +0100

    Update lib_funcs.h

--- lib_funcs.h
+++ lib_funcs.h
@@ -10,26 +10,53 @@
 #include <errno.h>
 #include <stdarg.h>
 
-extern char **__environ;
 extern char **environ;
 
+/* STR FUNCTIONS */
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
+int str2int(char *str);
+int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strdup(const char *s);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
-int _strncmp(const char *s1, const char *s2, size_t n);
+char *int2str(unsigned int n);
+
+/* SIGNAL HANDLING FUNCTIONS */
 void sigint_handler(int signum);
 void ststp_handler(int signum);
-char *int2str(unsigned int n);
-int str2int(char *str);
+
+/* COMMAND HANDLING/CONVERTING-TO-ARRAY FUNCTIONS */
 char **inpToArray(char *inp);
-char *_getenv(const char *name);
-void printerr(char *prgm, char* cmd_C, char *cmd, char *err, char *err1);
-void superFree_(char **_ARR_);
-int d_env();
 char **toArray(char *str, char *_sep_);
 char *toPath(char **_PDIR_, char *cmd, int *_notValidCMD_);
 char *onlyCMD(char *_cmd);
+
+/* ENVIRONMENT VARIABLES FUNCTIONS*/
+int d_env();
+char *_getenv(const char *name);
+
+/* PRINTING ERROR's */
+void printerr(char *prgm, char *cmd_C, char *cmd, char *err, char *err1);
+void superFree_(char **_ARR_);
+void frees(char ***PDIR, char ***ARGS, char **path, ...);
+
+/** CODE ASSEMBLING FUNCTIONS **/
+int __builtIn__(
+    char ***_PATHDIR_, char ***args, char ***argv,
+    char **path, char **_INPUT__, char **_str_cc_,
+    unsigned int *_cmd_count_);
+
+void __startForkin__(
+    pid_t *_pid,
+    char ***_PATHDIR_, char ***args, char ***env,
+    char **path, char **_INPUT__,
+    int *_status, int *_ch_exit_status);
+
+int __handleInvalidCMD__(
+    char ***_PATHDIR_, char ***args, char ***argv,
+    char **path, char **_str_cc_, char **_INPUT__, char **_PATH,
+    int *isValidCMD,
+    unsigned int *_cmd_count_);
 
 #endif
