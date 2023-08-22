0fe84d824d3c5d36442d847b700659a3e2dd32ef
Abderrahmane369 <133513357+Abderrahmane369@users.noreply.github.com>
2023-08-22 05:28:34 +0100

    Update free.c

--- free.c
+++ free.c
@@ -1,18 +1,46 @@
 #include "lib_funcs.h"
 
-/**
- * superFree_ - free args
- * @_ARR_: args.
- * return: nothing.
+/*
+ * free_args - free args
+ * @args: args
  */
+
 void superFree_(char **_ARR_)
 {
-char **a = _ARR_;
-while (*a)
-{
-free(*a);
-*a = NULL;
-a++;
+	char **a = _ARR_;
+
+	while (*a)
+	{
+		free(*a);
+		*a = NULL;
+		a++;
+	}
+
+	free(_ARR_);
 }
-free(_ARR_);
+
+void frees(char ***PDIR, char ***ARGS, char **path, ...)
+{
+	char **farg;
+
+	va_list vl;
+	va_start(vl, path);
+
+	superFree_(*PDIR);
+	*PDIR = (char **)NULL;
+	free(*path);
+	*path = NULL;
+	superFree_(*ARGS);
+	*ARGS = (char **)NULL;
+
+	farg = va_arg(vl, char **);
+
+	while (farg)
+	{
+		free(*farg);
+		*farg = NULL;
+		farg = va_arg(vl, char **);
+	}
+
+	va_end(vl);
 }
